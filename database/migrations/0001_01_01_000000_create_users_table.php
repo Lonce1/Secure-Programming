144d9a0fb09a8590f496382eb8e128bf6ef1dd02
LatteScott <bigsonagung@gmail.com>
2024-10-14 14:08:11 +0700

    Tes-1

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,31 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('confirfation password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('Blog_data', function(Blueprint $table){
-            $table->id();
-            $table->string('blog_name');
-            $table->timestamp('Blog created at')->nullable();
-            $table->string('creator name');
-            $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
@@ -51,9 +26,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('Blog_data');
         Schema::dropIfExists('sessions');
-        Schema::dropIfExists('password_reset_tokens');
+
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,9 +16,17 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::post('/login', [LoginController::class, 'login'])->middleware('throttle:5,1');
+Route::get('/login', [LoginController::class, 'viewLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
 Route::get('/register', function () {
     return view('register');
 });
+
+Route::get('/register', [RegisterController::class, 'viewRegisterForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
 use App\Http\Controllers\UserController;
 Route::get('users', [UserController::class, 'index']);
