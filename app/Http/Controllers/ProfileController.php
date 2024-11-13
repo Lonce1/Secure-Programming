fd65aeee12afd3e05644bea8a27690043758b0cf
LatteScott <bigsonagung@gmail.com>
2024-11-14 01:21:44 +0700

    profile picture database

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,13 +22,33 @@ class ProfileController extends Controller
                 'string',
                 'regex:/^8[0-9]{10}$/', // Ensures the number starts with 8 and is 11 digits long
                 'size:11'
+            
             ],
+            'profile_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Validate the profile image
         ]);
 
         $user = Auth::user();
         $user->email = $request->input('email');
         $user->username = $request->input('username');
         $user->phone_number = '+62' . $request->input('phone_number');
+        // if ($request->hasFile('profile_image')) {
+        //     if ($user->profile_image) {
+        //         Storage::delete($user->profile_image);
+        //     }
+        //     $path = $request->file('profile_image')->store('profile_images');
+        //     $user->profile_image = $path;
+        // }
+        if ($request->hasFile('profile_picture')) {
+            $profileImage = $request->file('profile_picture');
+            $imageName = time() . '.' . $profileImage->getClientOriginalExtension();
+            $profileImage->move(public_path('images/profile'), $imageName);
+            // Optionally delete old profile picture if exists
+            if ($user->profile_picture) {
+                unlink(public_path('images/profile/' . $user->profile_picture));
+            }
+
+            $user->profile_picture = $imageName;
+        }
         $user->save();
 
         return redirect('/home')->with('success', 'Profile updated successfully!');
